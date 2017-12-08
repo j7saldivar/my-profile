880e3c6fadcf91995abe8ecb72524320261647e4
Jorge Saldivar <j7saldivar@gmail.com>
2017-12-07 22:16:27 -0600

    Added new tomcat connector port 8080 and 8443 for production properties

---
+++ src/main/java/com/saldivar/config/TomcatConnectorConfig.java
@@ -0,0 +1,30 @@
+package com.saldivar.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class TomcatConnectorConfig {
+
+    @Profile(value = "production")
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        tomcat.addAdditionalTomcatConnectors(createHttpsConnector());
+        return tomcat;
+    }
+
+    private Connector createHttpsConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setSecure(false);
+        connector.setPort(8080);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
+
+}
