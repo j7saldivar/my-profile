bbc97548d78a11068bbd61e55e4e6f386e840ad4
Jorge Saldivar <1724724+jorgesaldivar@users.noreply.github.com>
2017-12-08 08:42:44 -0600

    Default server ports

--- src/main/java/com/saldivar/config/TomcatConnectorConfig.java
+++ src/main/java/com/saldivar/config/TomcatConnectorConfig.java
@@ -1,6 +1,7 @@
 package com.saldivar.config;
 
 import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class TomcatConnectorConfig {
+
+    @Value("${server.port.http:80}")
+    private int serverPortHttp;
+
+    @Value("${server.port.https:443}")
+    private int serverPortHttps;
 
     @Profile(value = "production")
     @Bean
@@ -22,8 +29,8 @@ public class TomcatConnectorConfig {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         connector.setScheme("http");
         connector.setSecure(false);
-        connector.setPort(8080);
-        connector.setRedirectPort(8443);
+        connector.setPort(serverPortHttp);
+        connector.setRedirectPort(serverPortHttps);
         return connector;
     }
 
