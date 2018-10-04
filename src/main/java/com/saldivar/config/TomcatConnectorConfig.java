fb0ff9ae6e6eea0752fae9c733979569807693b0
Jorge Saldivar <1724724+jorgesaldivar@users.noreply.github.com>
2018-10-04 15:17:49 -0500

    Updated spring boot version to 2.0.5 from 1.4.1

--- src/main/java/com/saldivar/config/TomcatConnectorConfig.java
+++ src/main/java/com/saldivar/config/TomcatConnectorConfig.java
@@ -1,9 +1,12 @@
 package com.saldivar.config;
 
+import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,27 +14,40 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 public class TomcatConnectorConfig {
 
-    @Value("${server.port.http:80}")
-    private int serverPortHttp;
+    private final Integer serverPortHttp;
+    private final Integer serverPortHttps;
 
-    @Value("${server.port.https:443}")
-    private int serverPortHttps;
+    public TomcatConnectorConfig(@Value("${server.port.http:80}") Integer serverPortHttp,
+                                 @Value("${server.port.https:443}") Integer serverPortHttps) {
+        this.serverPortHttp = serverPortHttp;
+        this.serverPortHttps = serverPortHttps;
+    }
 
     @Profile(value = "production")
     @Bean
-    public EmbeddedServletContainerFactory servletContainer() {
-        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
-        tomcat.addAdditionalTomcatConnectors(createHttpsConnector());
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
         return tomcat;
     }
 
-    private Connector createHttpsConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+    private Connector redirectConnector() {
+        Connector connector = new Connector(
+                TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
         connector.setScheme("http");
-        connector.setSecure(false);
         connector.setPort(serverPortHttp);
+        connector.setSecure(false);
         connector.setRedirectPort(serverPortHttps);
         return connector;
     }
-
 }
