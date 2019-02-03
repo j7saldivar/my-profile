2f16c496d2f7cf76109068314c108dc51f6c12f7
Jorge Saldivar <1724724+jorgesaldivar@users.noreply.github.com>
2019-02-02 21:22:31 -0600

    Added yearsOfExperience to session

--- src/main/java/com/jorgesaldivar/controller/HomeController.java
+++ src/main/java/com/jorgesaldivar/controller/HomeController.java
@@ -1,9 +1,13 @@
 package com.jorgesaldivar.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
 
 /**
  * Home page main controller
@@ -14,7 +18,10 @@ import java.security.Principal;
 public class HomeController {
 
     @RequestMapping({"/", "index"})
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute(
+                "yearsOfExperience",
+                Period.between(LocalDate.of(2013, Month.JUNE, 1), LocalDate.now()).getYears());
         return "index";
     }
 
